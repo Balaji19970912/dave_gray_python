b6aa6e3c6e28a7f51287878331fcb93ea6853af4
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2023-11-15 06:28:20 +0000

    list tuples dictionaries

---
+++ Day_01/lists.py
@@ -0,0 +1,8 @@
+list_one = [1,4,3,2]
+list_two = ['luffy','Zoro','Xer']
+
+list_one.sort()
+list_two.sort()
+
+print(list_one)
+print(list_two)
---
+++ Day_01/tuples.py
@@ -0,0 +1,4 @@
+first_tuple = (1,2,3,4,5,6)
+
+print(first_tuple.count(2))
+print(first_tuple.index(1))
